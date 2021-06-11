ca624fec8603d879512c9466c6ef5e3bfaa14b5e
raphasampaio <raphaelaraujosampaio@gmail.com>
2021-06-11 19:33:59 -0300

    Added initial sddp-preprocess script

--- sddp.jl/sddp-preprocess.lua
+++ sddp.jl/sddp-preprocess.lua
@@ -1,27 +1,42 @@
 local hydro = require("collection/hydro");
 
 -- MinBioSpillage
-hydro.min_bio_spillage_historical_scenarios
+local min_bio_spillage = hydro.min_bio_spillage_historical_scenarios
     :select_stages()
     :aggregate_blocks(BY_AVERAGE())
-    :save("foca1");
-    
--- MinSpillage
-hydro.min_spillage_historical_scenarios
-    :select_stages()
-    :aggregate_blocks(BY_AVERAGE())
-    :save("foca2");
+    :save_and_load("foca1");
 
-hydro.min_spillage_historical_scenarios_nodata
-    :select_scenarios(1)
+local min_bio_spillage_nodata = hydro.min_bio_spillage_historical_scenarios_nodata
     :select_stages()
-    :save("foca2_nodata");
+    :aggregate_blocks(BY_MAX())
+    :save_and_load("foca1_nodata");
+
+ifelse(min_bio_spillage_nodata:eq(1), -1, min_bio_spillage):save("foca1_onefile");
+
+-- MinSpillage
+local min_spillage = hydro.min_spillage_historical_scenarios
+    :select_stages()
+    :aggregate_blocks(BY_AVERAGE())
+    :save_and_load("foca2");
+
+local min_spillage_nodata = hydro.min_spillage_historical_scenarios_nodata
+    :select_stages()
+    :aggregate_blocks(BY_MAX())
+    :save_and_load("foca2_nodata");
+
+ifelse(min_spillage_nodata:eq(1), -1, min_spillage):save("foca2_onefile");
 
 -- VolumeMaximum
 hydro.vmax_chronological_historical_scenarios
     :select_stages()
     :aggregate_blocks(BY_LAST_VALUE())
     :save("foca3");
+
+-- VolumeMaximum
+hydro.vmax_chronological_historical_scenarios_nodata
+    :select_stages()
+    :aggregate_blocks(BY_LAST_VALUE())
+    :save("foca3_nodata");
 
 -- FloodControlStorage
 hydro.flood_volume_historical_scenarios
