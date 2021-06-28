09ed4d86bbfee517ffcfb57e344ae460db80b254
raphasampaio <raphaelaraujosampaio@gmail.com>
2021-06-28 10:32:29 -0300

    Updated brazil_supply_risk.lua

--- studies/brazil_supply_risk.lua
+++ studies/brazil_supply_risk.lua
@@ -1,17 +1,21 @@
 local is_sddp = true;
 
+local generic = require("collection/generic");
 local hydro = require("collection/hydro");
-local gerhid = hydro:load(is_sddp and "gerhid" or "gerhid_KTT"):aggregate_blocks(BY_SUM());
+
+local gerhid = hydro:load(is_sddp and "gerhid" or "gerhid_KTT"):convert("GWh"):aggregate_blocks(BY_SUM());
 
 local renewable = require("collection/renewable");
 local gergnd = renewable:load("gergnd"):aggregate_blocks(BY_SUM());
 
 local thermal = require("collection/thermal");
 local potter = thermal:load("potter"):convert("GWh"):aggregate_blocks(BY_SUM());
+potter:save("potter_test")
 
 local interconnection = require("collection/interconnection");
 local capint2 = interconnection:load("capint2"):convert("GWh"):aggregate_blocks(BY_SUM());
 local capint2_SE = capint2:select_agents({"SE -> NI", "SE -> NE", "SE -> NO"}):aggregate_agents(BY_SUM(), "SE");
+capint2_SE:save("capin2_se")
 
 local interconnection_sum = require("collection/interconnectionsum");
 local interc_sum = interconnection_sum.ub:select_agents({"Soma   14"}):aggregate_blocks(BY_AVERAGE()):convert("GWh");
@@ -20,7 +24,7 @@ capint2_SE = min(capint2_SE, interc_sum);
 
 local system = require("collection/system");
 local enearm = system:load(is_sddp and "enearm" or "storageenergy_aggregated");
-enearm = enearm:select_stages(2, enearm:stages()):reset_stages();
+enearm = is_sddp and enearm or enearm:select_stages(2, enearm:stages()):reset_stages();
 
 local earmzm = system:load(is_sddp and "earmzm" or "energystoragemax_aggregated");
 
@@ -39,23 +43,24 @@ generation:save("max_generation")
 local deficit = ifelse((demand - generation):gt(0), demand - generation, 0);
 
 -- DEFICIT SOMA DE MAIO A NOVEMBRO
-local deficit_sum = deficit:select_stages(1, 7):aggregate_stages(BY_SUM());
-deficit_sum:save("mismatch")
+mismatch_stages = deficit:select_stages(1, 5):rename_agents({"Mismatch - balanço hídrico"}):save_and_load("mismatch_stages");
+local deficit_sum = deficit:select_stages(1, 5):aggregate_stages(BY_SUM());
+mismatch = deficit_sum:save_and_load("mismatch");
 
-ifelse(deficit_sum:gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"mismatch risk"}):save("mismatch_risk");
+ifelse(deficit_sum:gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"Mismatch - balanço hídrico"}):save("mismatch_risk");
 
 -- DEFICIT RISCO A PRIORI
 -- ifelse(deficit_sum:gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"risk"}):save("deficit_risk");
 
 -- ENERGIA ARMAZENADA DO SUL E SUDESTE
-local enearm_SU = enearm:select_agents({"SUL"}):select_stages(7);
-local enearm_SE = enearm:select_agents({"SUDESTE"}):select_stages(7);
+local enearm_SU = enearm:select_agents({"SUL"}):select_stages(5);
+local enearm_SE = enearm:select_agents({"SUDESTE"}):select_stages(5);
 
 enearm_SU:save("enearm_SU_ini")
 enearm_SE:save("enearm_SE_ini")
 
-local earmzm_max_SU = earmzm:select_agents({"SUL"}):select_stages(7);
-local earmzm_max_SE = earmzm:select_agents({"SUDESTE"}):select_stages(7);
+local earmzm_max_SU = earmzm:select_agents({"SUL"}):select_stages(5);
+local earmzm_max_SE = earmzm:select_agents({"SUDESTE"}):select_stages(5);
 
 local earmzm_SU_level1 = earmzm_max_SU * 0.3;
 local earmzm_SE_level1 = earmzm_max_SE * 0.1;
@@ -72,15 +77,15 @@ local has_SE_level1_violation = enearm_SE:le(earmzm_SE_level1);
 local has_SU_level2_violation = enearm_SU:le(earmzm_SU_level2);
 local has_SE_level2_violation = enearm_SE:le(earmzm_SE_level2);
 
-ifelse(has_SU_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 1"}):save("enearm_risk_level1_SU");
-ifelse(has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 1"}):save("enearm_risk_level1_SE");
+ifelse(has_SU_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 1 (30%)"}):save("enearm_risk_level1_SU");
+ifelse(has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 1 (10%)"}):save("enearm_risk_level1_SE");
 ifelse(has_SU_level1_violation | has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE - level 1"}):save("enearm_risk_level1_SE_or_SU");
 
-ifelse(has_SU_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 2"}):save("enearm_risk_level2_SU");
-ifelse(has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 2"}):save("enearm_risk_level2_SE");
+ifelse(has_SU_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 2 (6%)"}):save("enearm_risk_level2_SU");
+ifelse(has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 2 (6%)"}):save("enearm_risk_level2_SE");
 ifelse(has_SU_level2_violation | has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE - level 2"}):save("enearm_risk_level2_SE_or_SU");
 
-local enearm2 = enearm:select_agents({"SUL", "SUDESTE"}):aggregate_agents(BY_SUM(), "SE and SU"):select_stages(7) - deficit_sum;
+local enearm2 = enearm:select_agents({"SUL", "SUDESTE"}):aggregate_agents(BY_SUM(), "SE and SU"):select_stages(5) - deficit_sum;
 -- ifelse(enearm2:lt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"final risk"}):save("deficit_final_risk");
 
 local p_prop_SU = earmzm_SU_level1 / (earmzm_SU_level1 + earmzm_SE_level1);
@@ -90,26 +95,6 @@ local enearm3 = max(enearm2, 0);
 
 local enearm_final_SU = enearm3 * p_prop_SU;
 local enearm_final_SE = enearm3 * p_prop_SE;
-
--- enearm_final_SU:save("enearm_final_SU");
--- enearm_final_SE:save("enearm_final_SE");
-
--- RISCO DE VIOLAÇÃO DOS NÍVEIS ONS A POSTERIORI
--- ifelse(enearm_final_SU:lt(earmzm_SU_level1), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL"}):save("enearm_final_risk_SU");
--- ifelse(enearm_final_SE:lt(earmzm_SE_level1), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE"}):save("enearm_final_risk_SE");
--- ifelse(enearm_final_SU:lt(earmzm_SU_level1) | enearm_final_SE:lt(earmzm_SE_level1), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE"}):save("enearm_final_risk_SE_or_SU");
-
-local function get_target_SU(current_energy, max_energy)
-	local meta_1 = max_energy * 0.06;
-	local meta_2 = max_energy * 0.30;
-	return ifelse(current_energy:gt(meta_2), meta_2, ifelse(current_energy:gt(meta_1), meta_1, 0.0))
-end
-
-local function get_target_SE(current_energy, max_energy)
-	local meta_1 = max_energy * 0.06;
-	local meta_2 = max_energy * 0.10;
-	return ifelse(current_energy:gt(meta_2), meta_2, ifelse(current_energy:gt(meta_1), meta_1, 0.0))
-end
 
 local function get_target(current_energy_se, max_energy_se, current_energy_su, max_energy_su)
 	local meta_su_1 = max_energy_su * 0.06;
@@ -151,9 +136,6 @@ deficit_sum:save("deficit_sum");
 for i = 1,3,1 do 
     print("iteration: " .. tostring(i) .. ": ")
 
-    -- local target_SE = get_target_SE(enearm_SE, earmzm_max_SE):save_and_load("target_SE_it" .. tostring(i));
-    -- local target_SU = get_target_SU(enearm_SU, earmzm_max_SU):save_and_load("target_SU_it" .. tostring(i));
-
     local target_SU, target_SE = get_target(enearm_SE, earmzm_max_SE, enearm_SU, earmzm_max_SU);
     print(target_SE);
     print(target_SU);
@@ -181,8 +163,6 @@ for i = 1,3,1 do
     local pode_esvaziar = enearm_SE - target_SE + enearm_SU - target_SU;
     pode_esvaziar:save_and_load("pode_esvaziar_it" .. tostring(i));
 
-    -- enearm_SE = ifelse(deficit_sum:gt(pode_esvaziar), target_SE, target_SE + (pode_esvaziar - deficit_sum) * (enearm_SE - target_SE)/pode_esvaziar):save_and_load("enearm_SE_it" .. tostring(i));
-    -- enearm_SU = ifelse(deficit_sum:gt(pode_esvaziar), target_SU, target_SU + (pode_esvaziar - deficit_sum) * (enearm_SU - target_SU)/pode_esvaziar):save_and_load("enearm_SU_it" .. tostring(i));
     local nao_pode_atender = deficit_sum:gt(pode_esvaziar:convert("GWh"));
     nao_pode_atender:save("nao_pode_atender_it" .. tostring(i));
     enearm_SE = ifelse(nao_pode_atender, target_SE, enearm_SE - deficit_sum * (enearm_SE - target_SE)/pode_esvaziar):save_and_load("enearm_SE_it" .. tostring(i));
@@ -192,23 +172,8 @@ end
 
 enearm_SE:rename_agents({"SUDESTE"}):save("enearm_SE_final");
 enearm_SU:rename_agents({"SUL"}):save("enearm_SU_final");
-deficit_sum:save("deficit_sum_final");
+deficit_sum:rename_agents({"Deficit"}):reset_stages():save("deficit_sum_final");
 
-
-
-
-
-
--- -- local p_prop_SU = earmzm_min_SU / (earmzm_min_SU + earmzm_min_SE);
--- -- local p_prop_SE = earmzm_min_SE / (earmzm_min_SU + earmzm_min_SE);
--- -- local enearm3 = max(enearm2, 0);
--- -- local enearm_final_SU = enearm3 * p_prop_SU;
--- -- local enearm_final_SE = enearm3 * p_prop_SE;
--- -- chama suprimento2.lua
---  -- (meta_SU, meta_SE, energia_atual_SU, energia_atual_SE, energia_max_SU,  energia_max_SE, deficit)
---  -- meta_SU e meta_SE vem da "descobre_metas"
---  -- energia_atual_SU e energia_atual_SE = enearm_SU e enearm_SE
---  -- energia_max_SU e  energia_max_SE = earmzm_max_SU e earmzm_max_SE
 deficit = deficit_sum
 
 earmzm_SE_level1:save("earmzm_SE_level1");
@@ -224,21 +189,52 @@ local has_SE_level2_violation = enearm_SE:le(earmzm_SE_level2);
 
 has_SE_level1_violation:save("has_SE_level1_violation");
 
-ifelse(has_SU_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 1"}):save("enearm_final_risk_level1_SU");
-ifelse(has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 1"}):save("enearm_final_risk_level1_SE");
-ifelse(has_SU_level1_violation | has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE - level 1"}):save("enearm_final_risk_level1_SE_or_SU");
+ifelse(has_SU_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - nível 1 (30%)"}):save("enearm_final_risk_level1_SU");
+ifelse(has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - nível 1 (10%)"}):save("enearm_final_risk_level1_SE");
+ifelse(has_SU_level1_violation | has_SE_level1_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE - nível 1"}):save("enearm_final_risk_level1_SE_or_SU");
 
-ifelse(has_SU_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - level 2"}):save("enearm_final_risk_level2_SU");
-ifelse(has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - level 2"}):save("enearm_final_risk_level2_SE");
+ifelse(has_SU_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL - nível 2 (6%)"}):save("enearm_final_risk_level2_SU");
+ifelse(has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUDESTE - nível 2 (6%)"}):save("enearm_final_risk_level2_SE");
 ifelse(has_SU_level2_violation | has_SE_level2_violation, 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"SUL or SUDESTE - level 2"}):save("enearm_final_risk_level2_SE_or_SU");
 
 
-ifelse(deficit_sum:gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"deficit risk"}):save("deficit_final_risk");
+ifelse(deficit_sum:gt(0), 1, 0):aggregate_scenarios(BY_AVERAGE()):convert("%"):rename_agents({"Deficit risk"}):reset_stages():save("deficit_final_risk");
 
+(mismatch_stages * (deficit_sum / mismatch)):save("Deficit_stages")
+
+
+local function get_percentile_chart(chart, filename, name_agent)    
+    local generic = require("collection/generic");
+    local output = generic:load(filename);
+
+    local selected = output;
+
+    local P = {1, 5, 10, 25, 50, 75, 90, 95, 99};
+
+    local label = filename .. "_"  .. "_avg";
+    selected:aggregate_scenarios(BY_AVERAGE()):rename_agents({name_agent .. " avg"}):save(label);
+    chart:push(label, "line");
+
+    local label = filename .. "_" .. "_min";
+    selected:aggregate_scenarios(BY_MIN()):rename_agents({name_agent .." min"}):save(label);
+    chart:push(label, "line");
+    
+    for _, p in ipairs(P) do 
+        local label = filename .. "_" .. "_p" .. tostring(p);
+        selected:aggregate_scenarios(BY_PERCENTILE(p)):rename_agents({name_agent .. " p" .. tostring(p)}):save(label);
+        chart:push(label, "line");
+    end
+
+    local label = filename .. "_" .. "_max";
+    selected:aggregate_scenarios(BY_MAX()):rename_agents({name_agent .. " max"}):save(label);
+    chart:push(label, "line");
+
+    return chart
+end
 
 local dashboard = Dashboard();
 
-local chart1 = Chart("enearm risk");
+local chart1 = Chart("Violação de volume mínimo - balanço hídrico");
 chart1:push("enearm_risk_level1_SU", "column");
 chart1:push("enearm_risk_level1_SE", "column");
 chart1:push("enearm_risk_level1_SE_or_SU", "column");
@@ -247,7 +243,7 @@ chart1:push("enearm_risk_level2_SE", "column");
 chart1:push("enearm_risk_level2_SE_or_SU", "column");
 dashboard:push(chart1);
 
-local chart2 = Chart("enearm final risk");
+local chart2 = Chart("Violação de volume mínimo");
 chart2:push("enearm_final_risk_level1_SU", "column");
 chart2:push("enearm_final_risk_level1_SE", "column");
 chart2:push("enearm_final_risk_level1_SE_or_SU", "column");
@@ -256,13 +252,17 @@ chart2:push("enearm_final_risk_level2_SE", "column");
 chart2:push("enearm_final_risk_level2_SE_or_SU", "column");
 dashboard:push(chart2);
 
-local chart3 = Chart("Deficit");
+local chart3 = Chart("Deficit risk");
 chart3:push("mismatch_risk", "column");
 chart3:push("deficit_final_risk", "column");
 dashboard:push(chart3);
 
+local chart4 = Chart("Mismatch - balanço hídrico");
+chart4 = get_percentile_chart(chart4, "mismatch_stages", "Mismatch - balanço hídrico");
+dashboard:push(chart4);
+
+local chart5 = Chart("Deficit");
+chart5 = get_percentile_chart(chart5, "Deficit_stages", "Deficit");
+dashboard:push(chart5);
+
 dashboard:save_style2("risk");
-
-
-
--- -- ifelse((earmzm_sul + earmzm_sudeste):lt(tmp), , )
