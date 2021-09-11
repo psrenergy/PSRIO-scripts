30100d28da0fefa90f5c673a57c39824052861e2
Luiz Carlos da Costa Junior <luizcarlos@psr-inc.com>
2021-09-11 20:46:51 -0300

    Update ./sddp/sddp-dashboard.lua

--- sddp/sddp-dashboard.lua
+++ sddp/sddp-dashboard.lua
@@ -1,14 +1,37 @@
-local costs = require("sddp/costs");
-local costs_aggregated = costs():aggregate_blocks(BY_SUM());
+local objcop = require("sddp/costs");
 
--- SDDPD_SCECOS
+-----------------------------------------------------------------------------------------------
+-- COSTS
+-----------------------------------------------------------------------------------------------
+local costs = ifelse(objcop():ge(0), objcop(), 0);
+
+-- sddp_dashboard_cost_tot
+costs:aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_cost_tot", {remove_zeros = true});
+
+-- sddp_dashboard_cost_avg
+costs:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_cost_avg", {remove_zeros = true});
+
+-- sddp_dashboard_cost_disp
 concatenate(
-    costs_aggregated:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
-    costs_aggregated:aggregate_agents(BY_SUM(), "P50"):aggregate_scenarios(BY_PERCENTILE(50)),
-    costs_aggregated:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
-):save("sddpd_scecos");
+    costs:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
+    costs:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
+    costs:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
+):save("sddp_dashboard_cost_disp");
 
--- SDDPD_PERCCOSTS
-local costs_per_agent = costs_aggregated:aggregate_scenarios(BY_AVERAGE());
-local costs_agents_aggregated = costs_per_agent:aggregate_agents(BY_SUM(), "total cost");
-(costs_per_agent / costs_agents_aggregated):convert("%"):save("sddpd_perccosts", {remove_zeros = true});
+-----------------------------------------------------------------------------------------------
+-- REVENUES
+-----------------------------------------------------------------------------------------------
+local revenues = ifelse(objcop():le(0), -objcop(), 0);
+
+-- sddp_dashboard_rev_tot
+revenues:aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_rev_tot", {remove_zeros = true});
+
+-- sddp_dashboard_rev_avg
+revenues:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_rev_avg", {remove_zeros = true});
+
+-- sddp_dashboard_rev_disp
+concatenate(
+    revenues:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
+    revenues:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
+    revenues:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
+):save("sddp_dashboard_rev_disp");
