420d806ebc40e666a58bd53a76e06cfda1639404
rsampaio <rsampaio@psr-inc.com>
2021-09-22 11:30:38 -0300

    Removed old files

--- power_analys_sddp.lua
+++
@@ -1,8 +0,0 @@
-local generic = require("collection/generic");
-
-local mismatch_power = generic:load("cache_mismatch_power");
-
-local apagao_severo = ifelse(mismatch_power:gt(0), 1, 0):aggregate_blocks(BY_AVERAGE());
-local risco_apagao_severo = ifelse(apagao_severo:gt(1), 1, 0):aggregate_scenarios(BY_AVERAGE());
-
-risco_apagao_severo:save("debug");
