0957bfcf480cbbf211bd7aeec2196ec202c56d56
Luiz Carlos da Costa Junior <luizcarlos@psr-inc.com>
2021-10-19 00:39:23 -0300

    Include calculation of top violations

--- sddp/sddp-dashboard.lua
+++ sddp/sddp-dashboard.lua
@@ -43,3 +43,56 @@ x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_
 if x > 0.0 then
     disp:save("sddp_dashboard_rev_disp");
 end
+
+-----------------------------------------------------------------------------------------------
+-- VIOLATIONS
+-----------------------------------------------------------------------------------------------
+local function violation_max(name)
+    n_agents = 5;
+
+    generic = Generic();
+    violation = generic:load(name);
+    if violation:loaded() then
+        violation = violation:aggregate_scenarios(BY_MAX()):aggregate_blocks(BY_MAX());
+
+        n = violation:agents_size();
+        if n > n_agents then
+            aux = violation:aggregate_stages(BY_MAX());
+            largest_agents = aux:select_largest_agents(n_agents):agents();
+
+            violation = concatenate(
+                violation:select_agents(largest_agents),
+                violation:remove_agents(largest_agents):aggregate_agents(BY_SUM(), "Others")
+            );
+        end
+        violation:save("sddp_dashboard_viol_" .. name, {remove_zeros = true});
+    end
+end
+
+violation_max("defcit");
+violation_max("defcitp");
+violation_max("nedefc");
+violation_max("defbus");
+violation_max("defbusp");
+violation_max("gncivio");
+violation_max("gncvio");
+violation_max("vrestg");
+violation_max("excbus");
+violation_max("excsis");
+violation_max("vvaler");
+violation_max("vioguide");
+violation_max("vriego");
+violation_max("vmxost");
+violation_max("vimxsp");
+violation_max("vdefmx");
+violation_max("vvolmn");
+violation_max("vdefmn");
+violation_max("vturmn");
+violation_max("vimnsp");
+violation_max("rampvio");
+violation_max("vreseg");
+violation_max("vsarct");
+violation_max("vsarhd");
+violation_max("vsarhden");
+violation_max("viocar");
+violation_max("vgmint");
