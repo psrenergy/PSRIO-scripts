fb5621fe78fa8e1c57afb95b1435b75ba54fff2b
Luiz Carlos da Costa Junior <luizcarlos@psr-inc.com>
2022-02-22 22:26:57 -0300

    Update sddp/sddp-dashboard.lua to include inflow energy dispersion chart

--- sddp/sddp-dashboard.lua
+++ sddp/sddp-dashboard.lua
@@ -1,8 +1,62 @@
-local objcop = require("sddp/costs");
+-----------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------------
+
+local function violation_aggregation(name,aggregation,suffix)
+    n_agents = 5;
+
+    generic = Generic();
+    violation = generic:load(name);
+    if violation:loaded() then
+        violation = violation:aggregate_scenarios(aggregation):aggregate_blocks(aggregation);
+
+        n = violation:agents_size();
+        if n > n_agents then
+            aux = violation:aggregate_stages(aggregation);
+            largest_agents = aux:select_largest_agents(n_agents):agents();
+
+            violation = concatenate(
+                violation:select_agents(largest_agents),
+                violation:remove_agents(largest_agents):aggregate_agents(BY_SUM(), "Others")
+            );
+        end
+        violation:save("sddp_dashboard_viol_" .. suffix .. "_" .. name, {remove_zeros = true, csv=true});
+    end
+end
+
+local function violation_output(names)
+    for i, name in ipairs(names) do
+--      Aggregation by Max
+        violation_aggregation(name,BY_MAX(),"max")
+--      Aggregation by Average
+        violation_aggregation(name,BY_AVERAGE(),"avg")
+    end
+end
+
+local function dispersion(output,file_name)
+    disp = concatenate(
+        output:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
+        output:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
+        output:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
+    );
+    x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_list()[1];
+    if x > 0.0 then
+        disp:save(file_name, {csv=true});
+    end
+end
+
+-----------------------------------------------------------------------------------------------
+-- INPUT DATA
+-----------------------------------------------------------------------------------------------
+-- Inflow energy
+local enaflu = Generic():load("enaflu");
+
+dispersion(enaflu,"sddp_dashboard_input_enaflu")
 
 -----------------------------------------------------------------------------------------------
 -- COSTS
 -----------------------------------------------------------------------------------------------
+local objcop = require("sddp/costs");
 local costs = ifelse(objcop():ge(0), objcop(), 0);
 
 -- sddp_dashboard_cost_tot
@@ -80,36 +134,5 @@ names_viol = {
 	"valeset",
 	"vespset"
 }
-
-local function violation_aggregation(name,aggregation,suffix)
-    n_agents = 5;
-
-    generic = Generic();
-    violation = generic:load(name);
-    if violation:loaded() then
-        violation = violation:aggregate_scenarios(aggregation):aggregate_blocks(aggregation);
-
-        n = violation:agents_size();
-        if n > n_agents then
-            aux = violation:aggregate_stages(aggregation);
-            largest_agents = aux:select_largest_agents(n_agents):agents();
-
-            violation = concatenate(
-                violation:select_agents(largest_agents),
-                violation:remove_agents(largest_agents):aggregate_agents(BY_SUM(), "Others")
-            );
-        end
-        violation:save("sddp_dashboard_viol_" .. suffix .. "_" .. name, {remove_zeros = true, csv=true});
-    end
-end
-
-local function violation_output(names)
-	for i, name in ipairs(names) do
---		Aggregation by Max
-		violation_aggregation(name,BY_MAX(),"max")
---		Aggregation by Average
-		violation_aggregation(name,BY_AVERAGE(),"avg")
-	end
-end
 
 violation_output(names_viol)
