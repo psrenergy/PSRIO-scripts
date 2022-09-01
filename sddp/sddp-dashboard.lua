09d43e3a66cb57e42725b5417237ef065471370f
Bruno Amaral <brunoamaral@psr-inc.com>
2022-09-01 00:38:10 -0300

    Adding conditions to check whether the expressions were loaded correctly or not

--- sddp/sddp-dashboard.lua
+++ sddp/sddp-dashboard.lua
@@ -51,7 +51,7 @@ local function dispersion(output,file_name)
         if x > 0.0 then
             disp:save(file_name, {csv=true});
         end
-    end 
+    end
 end
 
 -- Local function for discount rate calculation
@@ -74,21 +74,23 @@ dispersion(enaflu,"sddp_dashboard_input_enaflu")
 local objcop = require("sddp/costs");
 local costs = ifelse(objcop():ge(0), objcop(), 0);
 
--- sddp_dashboard_cost_tot. Considering discount rate in the cost aggregation
-(costs/discount_rate()):aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_cost_tot", {remove_zeros = true, csv=true});
-
--- sddp_dashboard_cost_avg
-costs:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_cost_avg", {remove_zeros = true, csv=true});
-
--- sddp_dashboard_cost_disp
-disp = concatenate(
-    costs:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
-    costs:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
-    costs:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
-);
-x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_list()[1];
-if x > 0.0 then
-    disp:save("sddp_dashboard_cost_disp", {csv=true});
+if( costs:loaded() ) then
+    -- sddp_dashboard_cost_tot. Considering discount rate in the cost aggregation
+    (costs/discount_rate()):aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_cost_tot", {remove_zeros = true, csv=true});
+    
+    -- sddp_dashboard_cost_avg
+    costs:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_cost_avg", {remove_zeros = true, csv=true});
+    
+    -- sddp_dashboard_cost_disp
+    disp = concatenate(
+        costs:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
+        costs:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
+        costs:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
+    );
+    x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_list()[1];
+    if x > 0.0 then
+        disp:save("sddp_dashboard_cost_disp", {csv=true});
+    end
 end
 
 -----------------------------------------------------------------------------------------------
@@ -96,21 +98,23 @@ end
 -----------------------------------------------------------------------------------------------
 local revenues = ifelse(objcop():le(0), -objcop(), 0);
 
--- sddp_dashboard_rev_tot. Considering discount rate in the revenue aggregation
-(revenues/discount_rate()):aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_rev_tot", {remove_zeros = true, csv=true});
-
--- sddp_dashboard_rev_avg
-revenues:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_rev_avg", {remove_zeros = true, csv=true});
-
--- sddp_dashboard_rev_disp
-disp = concatenate(
-    revenues:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
-    revenues:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
-    revenues:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
-);
-x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_list()[1];
-if x > 0.0 then
-    disp:save("sddp_dashboard_rev_disp", {csv=true});
+if( revenues:loaded() ) then
+    -- sddp_dashboard_rev_tot. Considering discount rate in the revenue aggregation
+    (revenues/discount_rate()):aggregate_scenarios(BY_AVERAGE()):aggregate_stages(BY_SUM()):save("sddp_dashboard_rev_tot", {remove_zeros = true, csv=true});
+    
+    -- sddp_dashboard_rev_avg
+    revenues:aggregate_scenarios(BY_AVERAGE()):save("sddp_dashboard_rev_avg", {remove_zeros = true, csv=true});
+    
+    -- sddp_dashboard_rev_disp
+    disp = concatenate(
+        revenues:aggregate_agents(BY_SUM(), "P10"):aggregate_scenarios(BY_PERCENTILE(10)),
+        revenues:aggregate_agents(BY_SUM(), "Average"):aggregate_scenarios(BY_AVERAGE()),
+        revenues:aggregate_agents(BY_SUM(), "P90"):aggregate_scenarios(BY_PERCENTILE(90))
+    );
+    x = disp:aggregate_agents(BY_SUM(), "CheckZeros"):aggregate_stages(BY_SUM()):to_list()[1];
+    if x > 0.0 then
+        disp:save("sddp_dashboard_rev_disp", {csv=true});
+    end
 end
 
 -----------------------------------------------------------------------------------------------
