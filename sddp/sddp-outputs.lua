82c37bf3078b4f291b192e7abd97e11c6fe86897
Bruno Amaral <brunoamaral@psr-inc.com>
2022-09-26 12:32:31 -0300

    Fix vere15 output in psrio script

--- sddp/sddp-outputs.lua
+++ sddp/sddp-outputs.lua
@@ -10,10 +10,10 @@ end
 local usernw = require("sddp/usernw");
 usernw():select_stages():save("usernw", {variable_by_block=2});
 
--- VEREC - Expected value of the percentage of rationing with respect to the load
+-- VERE15 - Expected value of the percentage of rationing with respect to the load
 local vere15 = require("sddp/vere15")();
 if vere15:is_hourly() then
-  enembp:save("vere15");
+  vere15:save("vere15");
 else
-  enembp:save("vere15",{csv=true});
+  vere15:save("vere15",{csv=true});
 end
