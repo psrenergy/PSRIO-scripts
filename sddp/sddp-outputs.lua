999e1bb590b7c83d5e74db89aa1d5c0c4b6cf74c
Bruno Amaral <brunoamaral@psr-inc.com>
2022-09-26 12:31:20 -0300

    Adding verec output to psrio script

--- sddp/sddp-outputs.lua
+++ sddp/sddp-outputs.lua
@@ -9,3 +9,11 @@ end
 -- USERNW - Renewable dispatch factor
 local usernw = require("sddp/usernw");
 usernw():select_stages():save("usernw", {variable_by_block=2});
+
+-- VEREC - Expected value of the percentage of rationing with respect to the load
+local vere15 = require("sddp/vere15")();
+if vere15:is_hourly() then
+  enembp:save("vere15");
+else
+  enembp:save("vere15",{csv=true});
+end
