5676cae89d1089879a3015d30970a082c1ba6d3e
Iury <iurysaboia927@gmail.com>
2023-10-09 10:15:20 -0300

    Update in MD script (script now is saving data in a .csv)

--- md/input_summary.lua
+++ md/input_summary.lua
@@ -249,7 +249,7 @@ local function create_prefix(label)
     return prefix;
 end
 
-local function demand_chart(case, tab)
+local function demand_data(case, tab)
     local demand = demand[case];
     local study  = study[case];
     local system = system[case];
@@ -260,26 +260,18 @@ local function demand_chart(case, tab)
     else
         demand_data = system.sensitivity * demand.inelastic_block:aggregate_agents(BY_SUM(), Collection.SYSTEM):aggregate_blocks(BY_SUM()):aggregate_stages(BY_SUM(),Profile.PER_YEAR);
     end
-    -- local inelastic_demand = ifelse(demand.is_elastic:eq(0),demand_data,0);
-    -- local elastic_demand = ifelse(demand.is_elastic:eq(1),demand_data,0);
 
     if demand_data:loaded() then
-        local chart = Chart();
-        chart:add_column(demand_data);
-        tab:push("## " .. dictionary.inelastic_demand[language]);
-        tab:push(chart);
+        -- local chart = Chart();
+        -- chart:add_column(demand_data);
+        -- tab:push("## " .. dictionary.inelastic_demand[language]);
+        -- tab:push(chart);
+        demand_data:save("demanda_MD",{fast_csv = true})
     end
-
-    -- if elastic_demand:loaded() then
-    --     local chart = Chart();
-    --     chart:add_column(elastic_demand,{color = colors.elastic_demand});
-    --     tab:push("## " .. dictionary.elastic_demand[language]);
-    --     tab:push(chart);
-    -- end
 
 end
 
-local function capacity_chart(case, tab)
+local function capacity_data(case, tab)
     local thermal = thermal[case];
     local hydro = hydro[case];
     local battery = battery[case];
@@ -303,36 +295,54 @@ local function capacity_chart(case, tab)
                              battery_capacity:loaded();
 
     if some_data_loaded then
-        local chart = Chart();
-        chart:add_categories(thermal_capacity, dictionary.thermal_capacity[language], {xLabel  = dictionary.system[language],
-                                                                 yLabel = dictionary.capacity[language]});
-        chart:add_categories(hydro_capacity,  dictionary.hydro_capacity[language]);
-        chart:add_categories(solar_capacity,  dictionary.solar_capacity[language]);
-        chart:add_categories(wind_capacity,  dictionary.wind_capacity[language]);
-        chart:add_categories(renewable_capacity,  dictionary.wind_capacity[language]);
-        chart:add_categories(csp_capacity,  dictionary.csp_capacity[language]);
-        chart:add_categories(battery_capacity,  dictionary.battery_capacity[language]);
-        tab:push("## " .. dictionary.capacity_by_tech[language]);
-        tab:push(chart);
+        -- local chart = Chart();
+        -- chart:add_categories(thermal_capacity, dictionary.thermal_capacity[language],
+        --                                                         {xLabel = dictionary.system[language],
+        --                                                          yLabel = dictionary.capacity[language]});
+        -- chart:add_categories(hydro_capacity,  dictionary.hydro_capacity[language]);
+        -- chart:add_categories(solar_capacity,  dictionary.solar_capacity[language]);
+        -- chart:add_categories(wind_capacity,  dictionary.wind_capacity[language]);
+        -- chart:add_categories(renewable_capacity,  dictionary.Others_renewables_capacity[language]);
+        -- chart:add_categories(csp_capacity,  dictionary.csp_capacity[language]);
+        -- chart:add_categories(battery_capacity,  dictionary.battery_capacity[language]);
+        -- tab:push("## " .. dictionary.capacity_by_tech[language]);
+        -- tab:push(chart);
 
-        local chart = Chart();
-        chart:add_pie(thermal_capacity:aggregate_agents(BY_SUM(),dictionary.thermal_capacity[language]));
-        chart:add_pie(hydro_capacity:aggregate_agents(BY_SUM(),dictionary.hydro_capacity[language]));
-        chart:add_pie(solar_capacity:aggregate_agents(BY_SUM(),dictionary.solar_capacity[language]));
-        chart:add_pie(wind_capacity:aggregate_agents(BY_SUM(),dictionary.wind_capacity[language]));
-        chart:add_pie(renewable_capacity:aggregate_agents(BY_SUM(),dictionary.wind_capacity[language]));
-        chart:add_pie(csp_capacity:aggregate_agents(BY_SUM(),dictionary.csp_capacity[language]));
-        chart:add_pie(battery_capacity:aggregate_agents(BY_SUM(),dictionary.battery_capacity[language]));
-        tab:push("## " .. dictionary.capacity_mix[language]);
-        tab:push(chart);
+        -- local chart = Chart();
+        -- chart:add_pie(thermal_capacity:aggregate_agents(BY_SUM(),dictionary.thermal_capacity[language]));
+        -- chart:add_pie(hydro_capacity:aggregate_agents(BY_SUM(),dictionary.hydro_capacity[language]));
+        -- chart:add_pie(solar_capacity:aggregate_agents(BY_SUM(),dictionary.solar_capacity[language]));
+        -- chart:add_pie(wind_capacity:aggregate_agents(BY_SUM(),dictionary.wind_capacity[language]));
+        -- chart:add_pie(renewable_capacity:aggregate_agents(BY_SUM(),dictionary.wind_capacity[language]));
+        -- chart:add_pie(csp_capacity:aggregate_agents(BY_SUM(),dictionary.csp_capacity[language]));
+        -- chart:add_pie(battery_capacity:aggregate_agents(BY_SUM(),dictionary.battery_capacity[language]));
+        -- tab:push("## " .. dictionary.capacity_mix[language]);
+        -- tab:push(chart);
     
+        local concatenate_data = {};
+        local concatenate_data_percent = {};
+        for _,system in ipairs(system:labels()) do
+            local concatenate_capacity = concatenate(thermal_capacity:select_agents({system}):add_suffix("_" .. dictionary.thermal_capacity[language]),
+                                                     hydro_capacity:select_agents({system}):add_suffix("_" .. dictionary.hydro_capacity[language]),
+                                                     solar_capacity:select_agents({system}):add_suffix("_" .. dictionary.solar_capacity[language]),
+                                                     wind_capacity:select_agents({system}):add_suffix("_" .. dictionary.wind_capacity[language]),
+                                                     renewable_capacity:select_agents({system}):add_suffix("_" .. dictionary.Others_renewables_capacity[language]),
+                                                     csp_capacity:select_agents({system}):add_suffix("_" .. dictionary.csp_capacity[language]),
+                                                     battery_capacity:select_agents({system}):add_suffix("_" .. dictionary.battery_capacity[language])
+                                                    );
+            local total_capacity = concatenate_capacity:aggregate_agents(BY_SUM(),"Total_Capacity");
+            table.insert(concatenate_data,concatenate_capacity);
+            table.insert(concatenate_data_percent,(concatenate_capacity/total_capacity):convert("%"));
+        end
+        concatenate(concatenate_data):save("capacity_MD",{fast_csv = true})
+        concatenate(concatenate_data_percent):save("capacity_MD_percent",{fast_csv = true})
     end
 
 end
 
-local dashboard<const> = Dashboard();
-local tab = create_tab(dictionary.tab_name[language],"book-open");
-demand_chart(1, tab)
-capacity_chart(1, tab)
-dashboard:push(tab);
-dashboard:save("dashboard");
+-- local dashboard<const> = Dashboard();
+-- local tab = create_tab(dictionary.tab_name[language],"book-open");
+demand_data(1, tab)
+capacity_data(1, tab)
+-- dashboard:push(tab);
+-- dashboard:save("dashboard");
