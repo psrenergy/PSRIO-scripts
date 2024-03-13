9173bed3d582b29c21eaf5b6a8e06ef72b84c108
Iury <iurysaboia927@gmail.com>
2024-03-13 19:08:09 -0300

    update script

---
+++ sddp/dlr-dashboard.lua
@@ -0,0 +1,17 @@
+
+local generic = Generic();
+
+local dprdash_psrio = generic:load("dprdashboard",true,true):force_hourly():force_unit("");
+
+local tab = Tab("DPR")
+
+local chart = Chart();
+chart:enable_controls();
+for stage = 1, dprdash_psrio:last_stage() do
+    chart:add_line(dprdash_psrio:select_stage(stage), { sequence = stage});
+end
+tab:push(chart);
+
+local dashboard = Dashboard()
+dashboard:push(tab)
+dashboard:save("dashboard_DLR")
