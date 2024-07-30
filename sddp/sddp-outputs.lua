e4dbb5b12cae5213709339e6932547f7c74a44cb
Iury <iurysaboia927@gmail.com>
2024-07-30 15:43:48 -0300

    Create reserve outputs

--- sddp/sddp-outputs.lua
+++ sddp/sddp-outputs.lua
@@ -91,3 +91,93 @@ if gerter:loaded() then
     gerter:convert("MW"):save("gerterMW", {csv=is_csv});
   end
 end
+
+-- RENMXRSER - Renewable max reserve
+renewable = Renewable();
+local renewable_reserv = renewable.max_reserve;
+if renewable_reserv:is_hourly() then
+  renewable_reserv:save("renmxreser", {variable_by_block=2, csv=is_csv});
+else
+  renewable_reserv:save("renmxreser", {csv=is_csv});
+end
+
+-- RENBDPRC - Renewable bid price
+renewable = Renewable();
+local renewable_bid = renewable.bid_price;
+if renewable_bid:is_hourly() then
+  renewable_bid:save("renbdprc", {variable_by_block=2, csv=is_csv});
+else
+  renewable_bid:save("renbdprc", {csv=is_csv});
+end
+
+-- BATMXRSER - Battery max reserve
+battery = Battery();
+local battery_reserv = battery.max_reserve;
+if battery_reserv:is_hourly() then
+  battery_reserv:save("batmxreser", {variable_by_block=2, csv=is_csv});
+else
+  battery_reserv:save("batmxreser", {csv=is_csv});
+end
+
+-- BATBDPRC - Battery bid price
+battery = Battery();
+local battery_bid = battery.bid_price;
+if battery_bid:is_hourly() then
+  battery_bid:save("batbdprc", {variable_by_block=2, csv=is_csv});
+else
+  battery_bid:save("batbdprc", {csv=is_csv});
+end
+
+-- HIDMXRSER - Hydro max reserve
+hydro = Hydro();
+local hydro_reserv = hydro.max_reserve;
+if hydro_reserv:is_loaded() then
+  local reserve_unit = hydro:load_vector("MaxSecondaryReserveUnit","");
+  local nominal_capacity_hydro_agents = reserve_unit:eq(2);
+  -- local mw_capacity_hydro_agents = reserve_unit:eq(3);
+  local avalailable_capacity_hydro_agents = reserve_unit:eq(14);
+
+  local nominal_capacity_hydro_reserv = hydro_reserv * nominal_capacity_hydro_agents * hydro.max_generation;
+  local avalailable_capacity_hydro_reserv = hydro_reserv * avalailable_capacity_hydro_agents * hydro.max_generation;
+  if hydro_reserv:is_hourly() then
+    (hydro_reserv + nominal_capacity_hydro_reserv + avalailable_capacity_hydro_reserv):save("hidmxreser", {variable_by_block=2, csv=is_csv});
+  else
+    (hydro_reserv + nominal_capacity_hydro_reserv + avalailable_capacity_hydro_reserv):save("hidmxreser", {csv=is_csv});
+  end
+end
+
+-- HIDBDPRC - Hydro bid price
+hydro = Hydro();
+local hydro_bid = hydro.bid_price;
+if hydro_bid:is_hourly() then
+  hydro_bid:save("hidbdprc", {variable_by_block=2, csv=is_csv});
+else
+  hydro_bid:save("hidbdprc", {csv=is_csv});
+end
+
+-- TERMXRSER - Thermal max reserve
+thermal = Thermal();
+local thermal_reserv = thermal.max_reserve;
+if thermal_reserv:is_loaded() then
+  local reserve_unit = thermal:load_vector("MaxSecondaryReserveUnit","");
+  local nominal_capacity_thermal_agents = reserve_unit:eq(2);
+  -- local mw_capacity_hydro_agents = reserve_unit:eq(3);
+  local avalailable_capacity_thermal_agents = reserve_unit:eq(14);
+
+  local nominal_capacity_thermal_reserv = thermal_reserv * nominal_capacity_thermal_agents * thermal.max_generation;
+  local avalailable_capacity_thermal_reserv = thermal_reserv * avalailable_capacity_thermal_agents * thermal.max_generation;
+  if thermal_reserv:is_hourly() then
+    (thermal_reserv + nominal_capacity_thermal_reserv + avalailable_capacity_thermal_reserv):save("termxreser", {variable_by_block=2, csv=is_csv});
+  else
+    (thermal_reserv + nominal_capacity_thermal_reserv + avalailable_capacity_thermal_reserv):save("termxreser", {csv=is_csv});
+  end
+end
+
+-- TERBDPRC - Thermal bid price
+thermal = Thermal();
+local thermal_bid = thermal.bid_price;
+if thermal_bid:is_hourly() then
+  thermal_bid:save("terbdprc", {variable_by_block=2, csv=is_csv});
+else
+  thermal_bid:save("terbdprc", {csv=is_csv});
+end
