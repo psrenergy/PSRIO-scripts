847d99210e10c1f2802df3eff2c8b54c8300aa69
Iury <iurysaboia927@gmail.com>
2025-07-07 12:40:42 -0300

    Fix translation error

---
+++ sddp/psrio.lua
@@ -0,0 +1,6 @@
+-- include("sddp-outputs.lua");
+-- include("sddp-reports.lua");
+include("sddp-warnings.lua");
+include("sddp-languages.lua");
+include("sddp-dashlib.lua");
+include("sddp-dashboard.lua");
