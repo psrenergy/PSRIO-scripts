47f937a9b3002a48d05907ba3c8084ac63ef3f41
Iury <iurysaboia927@gmail.com>
2025-07-07 12:40:56 -0300

    Revert "Fix translation error"
    This reverts commit 847d99210e10c1f2802df3eff2c8b54c8300aa69.

--- sddp/psrio.lua
+++
@@ -1,6 +0,0 @@
--- include("sddp-outputs.lua");
--- include("sddp-reports.lua");
-include("sddp-warnings.lua");
-include("sddp-languages.lua");
-include("sddp-dashlib.lua");
-include("sddp-dashboard.lua");
